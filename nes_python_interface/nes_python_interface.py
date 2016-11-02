24b8667e7e5a8d7a7560163fa5d6ce55f16f5052
Ferdinand Mathia <FerdinandPeer.Mathia@hs-augsburg.de>
2016-11-02 19:03:48 +0100

    moved ctypes code to module loading time, no longer is it per call

--- nes_python_interface/nes_python_interface.py
+++ nes_python_interface/nes_python_interface.py
@@ -11,6 +11,58 @@ import os
 
 nes_lib = cdll.LoadLibrary(os.path.join(os.path.dirname(__file__), 'libfceux.so'))
 
+nes_lib.NESInterface.argtypes = [c_char_p, c_bool]
+nes_lib.NESInterface.restype = c_void_p
+
+nes_lib.act.argtypes = [c_void_p, c_int]
+nes_lib.act.restype = c_int
+
+nes_lib.render.argtypes = [c_void_p]
+nes_lib.render.restype = None
+
+nes_lib.gameOver.argtypes = [c_void_p]
+nes_lib.gameOver.restype = c_bool
+
+nes_lib.resetGame.argtypes = [c_void_p]
+nes_lib.resetGame.restype = None
+
+nes_lib.getNumLegalActions.argtypes = [c_void_p]
+nes_lib.getNumLegalActions.restype = c_int
+
+nes_lib.getLegalActionSet.argtypes = [c_void_p, c_void_p]
+nes_lib.getLegalActionSet.restype = None
+
+nes_lib.getFrameNumber.argtypes = [c_void_p]
+nes_lib.getFrameNumber.restype = c_int
+
+nes_lib.lives.argtypes = [c_void_p]
+nes_lib.lives.restype = c_int
+
+nes_lib.getEpisodeFrameNumber.argtypes = [c_void_p]
+nes_lib.getEpisodeFrameNumber.restype = c_int
+
+nes_lib.getScreenHeight.argtypes = [c_void_p]
+nes_lib.getScreenHeight.restype = c_int
+nes_lib.getScreenWidth.argtypes = [c_void_p]
+nes_lib.getScreenWidth.restype = c_int
+
+nes_lib.getScreen.argtypes = [c_void_p, c_void_p, c_int]
+nes_lib.getScreen.restype = None
+
+nes_lib.fillRGBfromPalette.argtypes = [c_void_p, c_void_p, c_void_p, c_int]
+nes_lib.fillRGBfromPalette.restype = None
+
+nes_lib.getRam.argtypes = [c_void_p, c_void_p]
+nes_lib.getRam.restype = None
+
+nes_lib.saveState.argtypes = [c_void_p]
+nes_lib.saveState.restype = None
+
+nes_lib.loadState.argtypes = [c_void_p]
+nes_lib.loadState.restype = c_bool
+
+nes_lib.delete_NES.argtypes = [c_void_p]
+nes_lib.delete_NES.restype = None
 
 class NESInterface(object):
     def __init__(self, rom, eb_compatible=True, auto_render_period=-1):
@@ -19,8 +71,6 @@ class NESInterface(object):
         self.should_render = auto_render_period != -1
         self.auto_render_period = auto_render_period
         self.render_action_counter = 0
-        nes_lib.NESInterface.argtypes = [c_char_p, c_bool]
-        nes_lib.NESInterface.restype = c_void_p
         byte_string_rom = rom.encode('utf-8')
         self.obj = nes_lib.NESInterface(byte_string_rom, eb_compatible)
         self.width, self.height = self.getScreenDims()
@@ -47,8 +97,6 @@ class NESInterface(object):
     #     nes_lib.loadROM(self.obj, rom_file)
 
     def act(self, action):
-        nes_lib.act.argtypes = [c_void_p, c_int]
-        nes_lib.act.restype = c_int
         reward = nes_lib.act(self.obj, int(action))
         if self.should_render:
             if self.render_action_counter % self.auto_render_period == 0:
@@ -59,57 +107,39 @@ class NESInterface(object):
         return reward
 
     def render(self):
-        nes_lib.act.argtypes = [c_void_p]
-        nes_lib.act.restype = None
         return nes_lib.render(self.obj)
 
     def game_over(self):
-        nes_lib.gameOver.argtypes = [c_void_p]
-        nes_lib.gameOver.restype = c_bool
         return nes_lib.gameOver(self.obj)
 
     def reset_game(self):
-        nes_lib.resetGame.argtypes = [c_void_p]
-        nes_lib.resetGame.restype = None
         nes_lib.resetGame(self.obj)
 
     def getLegalActionSet(self):
-        nes_lib.getNumLegalActions.argtypes = [c_void_p]
-        nes_lib.getNumLegalActions.restype = c_int
         act_size = nes_lib.getNumLegalActions(self.obj)
         act = np.zeros(shape=(act_size,), dtype=c_int)
-        nes_lib.getLegalActionSet.argtypes = [c_void_p, c_void_p]
-        nes_lib.getLegalActionSet.restype = None
         nes_lib.getLegalActionSet(self.obj, as_ctypes(act))
         return act
 
     def getMinimalActionSet(self):
         # For NES we assume this is the same
         # as the legal actions.
+
+        # TODO Actually not true anymore but for now it works (yes, that old excuse ;))
         return self.getLegalActionSet()
 
     def getFrameNumber(self):
-        nes_lib.getFrameNumber.argtypes = [c_void_p]
-        nes_lib.getFrameNumber.restype = c_int
         return nes_lib.getFrameNumber(self.obj)
 
     def lives(self):
-        nes_lib.lives.argtypes = [c_void_p]
-        nes_lib.lives.restype = c_int
         return nes_lib.lives(self.obj)
 
     def getEpisodeFrameNumber(self):
-        nes_lib.getEpisodeFrameNumber.argtypes = [c_void_p]
-        nes_lib.getEpisodeFrameNumber.restype = c_int
         return nes_lib.getEpisodeFrameNumber(self.obj)
 
     def getScreenDims(self):
         """returns a tuple that contains (screen_width, screen_height)
         """
-        nes_lib.getScreenHeight.argtypes = [c_void_p]
-        nes_lib.getScreenHeight.restype = c_int
-        nes_lib.getScreenWidth.argtypes = [c_void_p]
-        nes_lib.getScreenWidth.restype = c_int
         width = nes_lib.getScreenWidth(self.obj)
         height = nes_lib.getScreenHeight(self.obj)
         return (width, height)
@@ -125,8 +155,6 @@ class NESInterface(object):
         if (screen_data is None):
             screen_data = np.zeros(self.width * self.height, dtype=np.uint8)
 
-        nes_lib.getScreen.argtypes = [c_void_p, c_void_p, c_int]
-        nes_lib.getScreen.restype = None
         nes_lib.getScreen(self.obj, as_ctypes(screen_data), c_int(screen_data.size))
         return screen_data
 
@@ -138,16 +166,12 @@ class NESInterface(object):
         """
         if (screen_data is None):
             screen_data = np.empty((self.height, self.width, 1), dtype=np.uint8)
-        nes_lib.getScreen.argtypes = [c_void_p, c_void_p, c_int]
-        nes_lib.getScreen.restype = None
 
         # First get the raw screen.
         nes_lib.getScreen(self.obj, as_ctypes(screen_data), c_int(screen_data.size))
 
         # Now convert to RGB.
         rgb_screen = np.empty((self.height, self.width, 3), dtype=np.uint8)
-        nes_lib.fillRGBfromPalette.argtypes = [c_void_p, c_void_p, c_void_p, c_int]
-        nes_lib.fillRGBfromPalette.restype = None
         nes_lib.fillRGBfromPalette(self.obj, as_ctypes(screen_data), as_ctypes(rgb_screen), c_int(screen_data.size))
         return rgb_screen
 
@@ -159,8 +183,6 @@ class NESInterface(object):
         """
         if (screen_data is None):
             screen_data = np.empty((self.height, self.width, 1), dtype=np.uint8)
-        nes_lib.getScreen.argtypes = [c_void_p, c_void_p, c_int]
-        nes_lib.getScreen.restype = None
         nes_lib.getScreen(self.obj, as_ctypes(screen_data[:]), c_int(screen_data.size))
         return screen_data
 
@@ -177,8 +199,6 @@ class NESInterface(object):
         if (ram is None):
             ram_size = self.getRAMSize()
             ram = np.zeros(ram_size, dtype=np.uint8)
-        nes_lib.getRam.argtypes = [c_void_p, c_void_p]
-        nes_lib.getRam.restype = None
         nes_lib.getRam(self.obj, as_ctypes(ram))
         return ram
 
@@ -188,14 +208,10 @@ class NESInterface(object):
 
     def saveState(self):
         """Saves the state of the system"""
-        nes_lib.saveState.argtypes = [c_void_p]
-        nes_lib.saveState.restype = None
         return nes_lib.saveState(self.obj)
 
     def loadState(self):
         """Loads the state of the system"""
-        nes_lib.loadState.argtypes = [c_void_p]
-        nes_lib.loadState.restype = c_bool
         return nes_lib.loadState(self.obj)
 
     def cloneState(self):
@@ -242,6 +258,4 @@ class NESInterface(object):
         return nes_lib.decodeState(as_ctypes(serialized), len(serialized))
 
     def __del__(self):
-        nes_lib.delete_NES.argtypes = [c_void_p]
-        nes_lib.delete_NES.restype = None
         nes_lib.delete_NES(self.obj)
